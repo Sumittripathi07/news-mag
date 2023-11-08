b8c18ca3a412e0c22c9e2db594ad588c1e4176b2
Sumit Tripathi <sumit.tpt07@gmail.com>
2023-11-08 23:54:11 +0530

    few fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import { useState } from "react";
 import Navbar from "./Components/Navbar";
 import Newsboard from "./Components/Newsboard";
 
-
 const App = () => {
-    const [category,setCategory]=useState("general");
-    return(
-        <div>
-            <Navbar setCategory={setCategory}/>
-            <Newsboard category={category}/>
-          
-        </div>
-    )
-}
+  const [category, setCategory] = useState("general");
+  return (
+    <div>
+      <Navbar setCategory={setCategory} />
+      <Newsboard category={category} />
+    </div>
+  );
+};
 
-export default App
+export default App;
